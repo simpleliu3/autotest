58803f2d4ebedf6a4b8ab21d7d0bbda2086670fa
simpleliu3 <simpliu3@163.com>
2019-06-05 16:15:50 +0800

    init

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from app.view import user,uitest,utils,apinew
+
+app = Flask(__name__)
+app.config.from_object('config')
+bootstrap = Bootstrap(app)
+app.register_blueprint(user.mod)
+app.register_blueprint(uitest.mod)
+app.register_blueprint(utils.mod)
+app.register_blueprint(apinew.mod)
+from app import views
